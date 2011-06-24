1548e99a8225e0a8533c29c3449e32e6e7e136c1
Yuning CHAI <chai.yuning@gmail.com>
2011-06-24 01:46:27 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nefarian::Application.config.secret_token = 'a655b913681b09973a77c1992d2a858c9db565298e4228cc69d8e99ba1be661d66d101aa1046711d06d8383a43dc33b80f0012f519950cc025d54784cb14d106'
