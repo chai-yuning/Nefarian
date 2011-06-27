a949b4fe0f274b3b7ac7f8cc5903c846e9d1bc51
Yuning CHAI <chai.yuning@gmail.com>
2011-06-27 01:58:37 +0100

    finished sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,81 @@
+# class User < ActiveRecord::Base
+  # attr_accessor :password
+  # attr_accessible :email, :name, :password, :password_confirmation
+#     
+  # email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  # validates :email, :presence => true,
+                    # :format => {:with => email_regex},
+                    # :uniqueness => {:case_sensitive => false} 
+#                     
+  # validates :name, :presence => true,
+                   # :length => {:maximum => 20}
+#                    
+  # validates :password, :presence => true,
+                       # :confirmation => true,
+                       # :length => {:within => 8..20}
+#                        
+  # before_save :encrypt_password
+# 
+  # def has_password?(submitted_password)
+    # encrypted_password == encrypt(submitted_password)
+  # end
+#   
+  # private
+# 
+    # def encrypt_password
+      # self.salt = secure_hash("#{Time.now.utc}--#{password}") if new_record?
+      # self.encrypted_password = encrypt(password)
+    # end
+# 
+    # def encrypt(s)
+      # secure_hash("#{salt}--#{s}")
+    # end
+# 
+    # def secure_hash(s)
+      # Digest::SHA2.hexdigest(s)
+    # end                     
+#                                               
+# end
+
+
+
 class User < ActiveRecord::Base
   
-  attr_accessible :email, :name
+  attr_accessor :password
+  attr_accessible :email, :name, :password, :password_confirmation
   
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
-  validate :email, :presence => true, 
-                   :format   => {:width => email_regex},
-                   :uniqueness => {:case_sensitive => false}
+  validates :email, :presence => true, 
+                    :format   => {:with => email_regex},
+                    :uniqueness => {:case_sensitive => false}                 
                    
-  validate :name, :presence => true,
-                  :length => {:maximum => 20}
+  validates :name, :presence => true,
+                   :length => {:maximum => 20}
+                  
+  validates :password, :presence => true,
+                       :confirmation => true,
+                       :length => {:within => 6..20}             
+                      
+  before_save :encrypt_password
   
+  def has_password?(submitted_password)
+    encrypted_password == encrypt(submitted_password)
+  end
+  
+  private 
+  
+    def encrypt_password
+      self.salt = secure_hash("#{Time.now.utc}--#{password}") if new_record?
+      self.encrypted_password = encrypt("#{salt}--#{password}")
+    end
+    
+    def encrypt(s)
+      secure_hash("#{salt}--#{s}")
+    end
+    
+    def secure_hash(s)
+      Digest::SHA2.hexdigest(s)
+    end   
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110626152630) do
+ActiveRecord::Schema.define(:version => 20110626163558) do
 
   create_table "users", :force => true do |t|
     t.string   "email"
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "encrypted_password"
+    t.string   "salt"
   end
 
 end
