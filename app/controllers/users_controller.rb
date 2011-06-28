1c2a5a32ed840c51396548605f0d204556cb1d24
Yuning CHAI <chai.yuning@gmail.com>
2011-06-28 11:05:26 +0100

    added session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      render 'show'
+      sign_in @user
+      flash[:success] = "Welcome"
+      redirect_to @user
     else
       @title = "Sign up"
       render 'new'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Nefarian::Application.routes.draw do
 
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
+  
+  root :to => 'sessions#new'
+  match '/signup', :to => 'users#new'
+  match '/signin', :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
 
 end
